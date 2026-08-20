c76e1e40135b5ac67782890978b1ddba10add170
i <isomorphisms@sdf.org>
2026-08-19 23:24:34 -0400

    Fix Claim equality decision proof under Rocq 9.2

--- TrustedMetatheory.v
+++ TrustedMetatheory.v
@@ -12,7 +12,7 @@ Inductive Claim : Type :=
 
 Definition claim_eq_dec : forall x y : Claim, {x = y} + {x <> y}.
 Proof.
-  decide equality.
+  decide equality; decide equality.
 Defined.
 
 Inductive derives (premises : list Claim) : Claim -> Prop :=
